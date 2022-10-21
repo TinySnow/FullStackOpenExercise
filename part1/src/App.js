3ac5c616d86285495cbb44c363279653ce26888f
TinySnow <lo.tinysnow.ol@outlook.com>
2022-10-21 17:20:32 +0800

    part1.a done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,3 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-
-  )
-}
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1}></Part>
-      <Part part={props.part2} exercises={props.exercises2}></Part>
-      <Part part={props.part3} exercises={props.exercises3}></Part>
-    </>
-  )
-}
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -36,11 +9,17 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
--- part1/src/index.js
+++ part1/src/index.js
@@ -2,5 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import App_1 from './App_1'
+import App_2 from './App_2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('one')).render(<App_1 />)
+ReactDOM.createRoot(document.getElementById('two')).render(<App_2 />)
